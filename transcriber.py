bd0c059e551f99e356d0a6ecbf0fe907388ff3be
Joyce Katherine K <joycekatherinek2509@gmail.com>
2026-07-14 22:03:23 +0530

    Fix Streamlit deployment

--- transcriber.py
+++ transcriber.py
@@ -1,12 +1,22 @@
+import streamlit as st
 from faster_whisper import WhisperModel
-model = WhisperModel(
-    "base",
-    device="cpu",
-    compute_type="int8"
-)
+
+@st.cache_resource
+def load_model():
+    return WhisperModel(
+        "base",
+        device="cpu",
+        compute_type="int8"
+    )
+
 def transcribe_audio(file):
+    model = load_model()
+
     segments, info = model.transcribe(file)
+
     transcript = ""
+
     for segment in segments:
         transcript += segment.text + " "
+
     return transcript
